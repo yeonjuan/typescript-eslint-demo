dcfde80b86c43c93ec9352acdae11a1f16ebeb5f
yeonjuan <yeonjuan93@naver.com>
2020-10-12 19:37:26 +0900

    feat: add constants

---
+++ src/components/constants.ts
@@ -0,0 +1,24 @@
+import type { Linter } from "eslint";
+
+export const DEFAULT_CODE = `
+async function invalidInTryCatch1() {
+  try {
+    return Promise.resolve('try');
+  } catch (e) {}
+}
+`;
+
+export const DEFAULT_RULE_CONFIG: Linter.RulesRecord = {
+  "@typescript-eslint/return-await": "error",
+};
+
+export const ECMA_VERSIONS = [
+  "es2015",
+  "es2016",
+  "es2017",
+  "es2018",
+  "es2019",
+  "es2020",
+] as const;
+
+export const ECMA_FEATURES = ["jsx", "globalReturn"] as const;
