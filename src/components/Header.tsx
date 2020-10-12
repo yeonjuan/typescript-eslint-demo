4da07381416817b408ded899045a5edf6fd97481
yeonjuan <yeonjuan93@naver.com>
2020-10-12 21:38:23 +0900

    feat: add typescript-eslint, typescript-version

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import type { FC } from "react";
 import "@/css/header.css";
+import pkgJson from "../../package.json";
 
 export const Header: FC = () => (
   <header>
     <h2 style={{ color: "#666" }}>TypeScript ESlint Demo</h2>
+    <ul className="versions">
+      <li>
+        TypeScript-ESLint v
+        {pkgJson.dependencies["@typescript-eslint/eslint-plugin"]}
+      </li>
+      <li>TypeScript v{pkgJson.dependencies["typescript"]}</li>
+    </ul>
   </header>
 );
