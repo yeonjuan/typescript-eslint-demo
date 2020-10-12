5e82289ef21c2c31d5324ca0cecbcf304c8a20a3
yeonjuan <yeonjuan93@naver.com>
2020-10-12 22:21:49 +0900

    feat: add version in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,50 @@
 import React from "react";
+import { Navbar, Badge } from "react-bootstrap";
 import type { FC } from "react";
 import "@/css/header.css";
 import pkgJson from "../../package.json";
 
 export const Header: FC = () => (
-  <header>
-    <h2 style={{ color: "#666" }}>TypeScript ESlint Demo</h2>
-    <ul className="versions">
-      <li>
-        TypeScript-ESLint v
-        {pkgJson.dependencies["@typescript-eslint/eslint-plugin"]}
-      </li>
-      <li>TypeScript v{pkgJson.dependencies["typescript"]}</li>
-    </ul>
-  </header>
+  <Navbar bg="light">
+    <Navbar.Brand>
+      TypeScript ESLint Demo
+      <Badge variant="secondary">v{pkgJson.version}</Badge>
+    </Navbar.Brand>
+
+    <Navbar.Collapse className="justify-content-end">
+      <Navbar.Text>
+        <a
+          className="github-button"
+          href="https://github.com/yeonjuan/typescript-eslint-demo"
+          data-icon="octicon-star"
+          aria-label="Star yeonjuan/typescript-eslint-demo on GitHub"
+        >
+          Star
+        </a>
+      </Navbar.Text>
+      <Navbar.Text>
+        <a
+          className="github-button"
+          href="https://github.com/yeonjuan/typescript-eslint-demo/issues"
+          data-icon="octicon-issue-opened"
+          aria-label="Issue yeonjuan/typescript-eslint-demo on GitHub"
+        >
+          Issue
+        </a>
+      </Navbar.Text>
+      <ul className="version-list">
+        <li>
+          <Navbar.Text className="version">
+            TypeScript-ESLint v
+            {pkgJson.dependencies["@typescript-eslint/eslint-plugin"]}
+          </Navbar.Text>
+        </li>
+        <li>
+          <Navbar.Text className="version">
+            TypeScript v{pkgJson.dependencies["typescript"]}
+          </Navbar.Text>
+        </li>
+      </ul>
+    </Navbar.Collapse>
+  </Navbar>
 );
