dd1286f975207858e121e215102c7fe0094f3a7d
yeonjuan <yeonjuan93@naver.com>
2020-10-12 19:35:50 +0900

    feat: add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import type { FC } from "react";
+import "@/css/header.css";
+
+export const Header: FC = () => (
+  <header>
+    <h2 style={{ color: "#666" }}>TypeScript ESlint Demo</h2>
+  </header>
+);
